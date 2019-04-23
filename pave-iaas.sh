13f3f9c3494695c72dbdcadab3c1525cb285c567
pvtl-dnoguerol <dnoguerol@pivotal.io>
2019-04-23 15:38:49 +0200

    Update

--- pave-iaas.sh
+++ pave-iaas.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 find .
-cp terraform-tfvars-s3/terraform-1.0.0.tfvars terraforming-aws/terraforming-pas/terraform.tfvars
+cp terraform-tfvars-s3/terraform-1.0.1.tfvars terraforming-aws/terraforming-pas/terraform.tfvars
 cd terraforming-aws/terraforming-pas
+echo "Performing terraform init\n"
 terraform init -input=false
+echo "Performing terraform apply\n"
 terraform apply -input=false
