7e5293eed643c6b58bddf9d257576b29d008bab0
pvtl-dnoguerol <dnoguerol@pivotal.io>
2019-04-24 15:57:05 +0200

    Updated

--- create-opsman-config.sh
+++ create-opsman-config.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-find .
+
 cat terraform-output/terraform.out
 
 cp terraform-output/terraform.out opsman-output/terraform.out
+
+find .
--- pave-iaas.sh
+++ pave-iaas.sh
@@ -5,6 +5,8 @@ cp terraform-tfvars-s3/terraform-1.0.3.tfvars terraforming-aws/terraforming-pas/
 cd terraforming-aws/terraforming-pas
 
 touch ../../terraform-output/terraform.out
+
+date >>  ../../terraform-output/terraform.out
 #echo "Performing terraform init"
 #terraform init -input=false
 #echo "Performing terraform apply"
