acc5a3da7cebfba09e620c1786a40a2a6719631a
pvtl-dnoguerol <dnoguerol@pivotal.io>
2019-04-24 15:49:02 +0200

    Updated

--- pave-iaas.sh
+++ pave-iaas.sh
@@ -3,7 +3,9 @@
 find .
 cp terraform-tfvars-s3/terraform-1.0.3.tfvars terraforming-aws/terraforming-pas/terraform.tfvars
 cd terraforming-aws/terraforming-pas
-echo "Performing terraform init"
-terraform init -input=false
-echo "Performing terraform apply"
-terraform apply -input=false -auto-approve > ../../terraform-output/terraform.out
+
+touch ../../terraform-output/terraform.out
+#echo "Performing terraform init"
+#terraform init -input=false
+#echo "Performing terraform apply"
+#terraform apply -input=false -auto-approve > ../../terraform-output/terraform.out
