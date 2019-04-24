f39c38aeddb11886e370d41773b30f6fdb4b070e
pvtl-dnoguerol <dnoguerol@pivotal.io>
2019-04-24 17:55:39 +0200

    Updated

--- create-opsman-config.sh
+++ create-opsman-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
-apt-get install -y python3
+apt-get install -y python2
 python3 platform-automation-test/parse-terraform-output.py terraform-output/terraform.1.0.0.out > opsman-output/opsman-config-1.0.0.yml
 find .
